de847130a9002dbf73c0a595f16e742b13512cb8
haroonsiddiqqureshi <haroonsiddiq.q@kkumail.com>
2024-09-01 23:09:22 +0700

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,10 @@
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="p-4 flex h-screen justify-center flex-col items-center">
       <h1 className="text-7xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-        Welcome to my website
+           ชื่อ นาย ประจักษ์ ศรีทอง <br/>
+           รหัสนักศึกษา 653450294-0
       </h1>
     </main>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="bg-stone-800 p-4 text-white">
+          <ul className="flex gap-2">
+            <li>
+              <Link href="/">
+              Home 
+              </Link>
+            </li> 
+            <li>
+              <Link href="/about">
+              About 
+              </Link>
+            </li> 
+            <li>
+              <Link href="/pokemon">
+              Pokemon 
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {children}</body>
     </html>
   );
 }
