0d39e84ab6cd486876c197cef072c4485fa2abcf
haroonsiddiqqureshi <haroonsiddiq.q@kkumail.com>
2024-09-01 23:23:33 +0700

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,9 @@
 export default function Page() {
   return (
     <main className="p-4 flex h-screen justify-center flex-col items-center">
-      <h1 className="text-7xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-           ชื่อ นาย ประจักษ์ ศรีทอง <br/>
-           รหัสนักศึกษา 653450294-0
+      <h1 className="text-7xl">
+        นายฮารูณ ซิดดิ๊ก คูเรซิ  <br />
+        รหัสนักศึกษา 653450294-0
       </h1>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 export default function Home() {
   return (
     <main className="p-4 flex h-screen justify-center flex-col items-center">
-      <h1 className="text-7xl bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
-        Welcome to my website
+      <h1 className="text-7xl">
+        Welcome to nextjs project
       </h1>
     </main>
   );
